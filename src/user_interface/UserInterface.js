b63df7c3246a21dfb47f7cc1c4e4995e1e2a2516
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2021-02-03 13:32:58 -0500

    Loaded countries into select list. Next step: create generic select list component that can be used for country, year, and calc price

--- src/user_interface/UserInterface.js
+++ src/user_interface/UserInterface.js
@@ -1,16 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { makeHashKey } from '../helpers/utilities';
 import { loadCancers } from '../state/slices/cancersSlice';
 import { loadSetting } from '../state/slices/settingSlice';
 import { loadPrices } from '../state/slices/costsSlice';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import StaticTitle from './StaticTitle';
 import CancerButtons from './CancerButtons';
 import UserInputs from './UserInputs';
 
-const UserInterface = ({ setVisible, loadAllCosts }) => {
+const UserInterface = ({ setVisible, loadAllCosts, uiLabels }) => {
   const dispatch = useDispatch();
 
   const names = useSelector( (state) => {
@@ -53,28 +55,52 @@ const UserInterface = ({ setVisible, loadAllCosts }) => {
   return (
     <div>
       <Container>
-        <h2>Temporary static inputs</h2>
+        <h2>User inputs</h2>
         <header role="header">
-          <Row>
-            <Col>
-              <StaticTitle 
-                heading={ "Country" }
-                text={ setting.name }
-              />
-            </Col>
-            <Col>
-              <StaticTitle 
-                heading={ "Year" }
-                text={ setting.year }
-              />
-            </Col>
-            <Col>
-              <StaticTitle 
-                heading={ "Calculation source" }
-                text={ priceType }
-              />
-            </Col>
-          </Row>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group>
+                  <h3>Country</h3>
+                  <Form.Label>Select a country from the list below</Form.Label>
+                  <Form.Control
+                    as="select"
+                    name="countries"
+                  >
+                    <option value="0">--Select a country--</option>
+                    { uiLabels.countries.map( (country, i) => {
+                      return (
+                        <option 
+                          key={ i }
+                          value={ country }
+                          name={ country }
+                        >{ country }
+                        </option>
+                      );
+                    })}
+                  </Form.Control>
+                </Form.Group>
+              {/*
+                <StaticTitle 
+                  heading={ "Country" }
+                  text={ setting.name }
+                />
+              */}
+              </Col>
+              <Col>
+                <StaticTitle 
+                  heading={ "Year" }
+                  text={ setting.year }
+                />
+              </Col>
+              <Col>
+                <StaticTitle 
+                  heading={ "Calculation source" }
+                  text={ priceType }
+                />
+              </Col>
+            </Row>
+          </Form>
           <Button onClick={ loadDefaultCountryYear }>Load Argentina 2020 values</Button>
           <Button onClick={ loadDefaultPriceType }>Load consolidated prices</Button>
         </header>
