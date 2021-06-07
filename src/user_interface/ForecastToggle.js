b73a057e16b5db64e4baeee399782d9051713f28
Dave Rothfarb <dave.rothfarb.webdev@gmail.com>
2021-06-07 13:54:04 -0400

    Reverted setting toggle to include Health System, which was previously disabled for user testing on Single Institution

--- src/user_interface/ForecastToggle.js
+++ src/user_interface/ForecastToggle.js
@@ -1,64 +1,27 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import ToggleButton from 'react-bootstrap/ToggleButton';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Overlay from 'react-bootstrap/Overlay';
-import Tooltip from 'react-bootstrap/Tooltip';
 import { sentenceCase } from '../helpers/utilities';
 
 const ForecastToggle = ({ name, extraClass, labels, saved, handleChange }) => {
-  const [show, setShow] = useState(false);
-  const target = useRef(null);
-
   return (
     <div className={ `d-flex justify-content-center ${ extraClass }` }>
       <ButtonGroup toggle size="lg"> 
         { labels.map( (label, i) => {
           return (
-            label === "Health system" ?
-              <React.Fragment key={ i }>
-                <ToggleButton 
-                  bsPrefix="btn btn-primary disabled"
-                  name={ name }
-                  value={ label } 
-                  checked={ saved === label }
-                  type="radio"
-                  key={ i } 
-                  ref={ target }
-                  onChange={ () => setShow(!show) } 
-                >
-                  { label }
-                </ToggleButton>
-                <Overlay target={ target.current } show={ show }>
-                  <Tooltip>
-                    This feature is temporarily disabled. Please choose 'Single institution'.
-                  </Tooltip>
-                </Overlay>
-              </React.Fragment>
-            :
-            <ToggleButton 
-              name={ name }
-              value={ label } 
-              checked={ saved === label }
-              type="radio"
-              key={ i } 
-              onChange={ (e) => { setShow(false); handleChange(e); } } 
-            >
-              { label }
-            </ToggleButton>
-        /**
-         * Temporarily changed to alpha test Single institution only
-         *
+            <React.Fragment key={ i }>
               <ToggleButton 
+                bsPrefix="btn btn-primary"
                 name={ name }
                 value={ label } 
                 checked={ saved === label }
                 type="radio"
                 key={ i } 
-                onChange={ handleChange} 
+                onChange={ handleChange } 
               >
                 { label }
               </ToggleButton>
-        */
+            </React.Fragment>
           );
         })}
       </ButtonGroup>
