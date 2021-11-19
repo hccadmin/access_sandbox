759fb412ef40b120921d0e4280f67c32b24654c1
David Rothfarb <dave_rothfarb@dfci.harvard.edu>
2021-11-19 14:13:38 -0500

    Trying to fix stupid findDOMNode error

--- src/shared_components/HelpTextPopover.js
+++ src/shared_components/HelpTextPopover.js
@@ -5,10 +5,9 @@ import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 const HelpTextPopover = ({ title, content, children }) => {
   return (
     <OverlayTrigger
-      trigger="hover"
       placement="top"
       overlay={
-        <Popover>
+        <Popover id={ title }>
           <Popover.Title as="h3">{ title }</Popover.Title>
           <Popover.Content>{ content }</Popover.Content>
         </Popover>
